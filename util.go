4307533065520b74d717695d9611729021fc65c5
deadblue <9deadblue@gmail.com>
2018-06-24 19:27:32 +0800

    add missed util.go

---
+++ util.go
@@ -0,0 +1,23 @@
+package telegroid
+
+func isUpper(ch rune) bool {
+	return ch >= 'A' && ch <= 'Z'
+}
+
+func toLower(ch rune) rune {
+	if isUpper(ch) {
+		return ch - ('A' - 'a')
+	} else {
+		return ch
+	}
+}
+
+func toMethodName(name string) string {
+	if len(name) == 0 {
+		return ""
+	} else {
+		runes := []rune(name)
+		runes[0] = toLower(runes[0])
+		return string(runes)
+	}
+}
