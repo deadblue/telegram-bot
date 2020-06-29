ce4b6213ba3993e05c4bb962b7650d03a01fe72f
deadblue <root@dead.blue>
2020-06-29 18:04:21 +0800

    Update example codes.

--- example_test.go
+++ example_test.go
@@ -1,17 +1,40 @@
 package telegram
 
-import "log"
+import (
+	"github.com/deadblue/telegram-bot/parameters"
+	"github.com/deadblue/telegram-bot/parameters/text"
+	"log"
+)
 
 const (
 	telegramBotToken = "your-bot-token"
 )
 
-func Example() {
+func ExampleBot_GetMe() {
+	// Create bot
 	bot := New(telegramBotToken)
+	// Call API
 	user, err := bot.GetMe()
 	if err != nil {
-		log.Fatalf("Call getMe error: %s", err.Error())
+		log.Fatalf("Call getMe error: %s", err)
 	} else {
 		log.Printf("Bot name: %s", user.Username)
+	}
+}
+
+func ExampleBot_SendMessage() {
+	// Create bot
+	bot := New(telegramBotToken)
+
+	// Send text message
+	params := new(parameters.SendMessageParams)
+	params.ChatId(12345678)
+	params.Text(text.MarkdownV2("Hello, world"))
+	params.DisableNotification()
+	msg, err := bot.SendMessage(params)
+	if err != nil {
+		log.Fatalf("Call sendMessage error: %s", err)
+	} else {
+		log.Printf("Sent message: %s", msg.Text)
 	}
 }
